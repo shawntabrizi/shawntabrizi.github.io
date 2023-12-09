09610ebdc2410c4267922a60af1486185bad936a
Shawn Tabrizi <shawntabrizi@gmail.com>
2023-12-08 20:42:14 -0400

    update social meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description="a site about discovery through experience"
     >
       <main className={styles.heroBanner}>
         <div className="container">
